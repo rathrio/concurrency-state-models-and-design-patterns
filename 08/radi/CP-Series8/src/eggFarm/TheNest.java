d93f77280578155871b3c60b0079e5aa6859ec8b
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-11-15 08:21:05 +0100

    Add 1,2,3

--- 08/radi/CP-Series8/src/eggFarm/TheNest.java
+++ 08/radi/CP-Series8/src/eggFarm/TheNest.java
@@ -13,29 +13,44 @@ public class TheNest implements Buffer {
 	protected int out = 0;
 	protected int size;
 
-	protected Semaphore full;  // counts number of items
-	protected Semaphore empty; // counts number of spaces
+	// Renamed the semaphores to make it more readable to me. They're still
+	// used the same.
+	protected Semaphore usedSlots;  // counts number of items
+	protected Semaphore emptySlots; // counts number of spaces
 
 	public TheNest(int size) {
 		this.size = size;
 		this.buf = new Object[size];
-		this.full = new Semaphore(0);
-		this.empty = new Semaphore(size);
+		this.usedSlots = new Semaphore(0);
+		this.emptySlots = new Semaphore(size);
 	}
 
-	public synchronized void put(Object obj) {
-		empty.down();
-		buf[in] = obj;
-		in = (in + 1) % size;
-		full.up();
+	// No longer synchronized!
+	public void put(Object obj) {
+		emptySlots.down();
+		
+		// But here it is. So we reverse the synchronization order. There's no
+		// need for a guard loop I believe, because the semaphore above 
+		// guarantees that we "haven't missed the signal".
+		synchronized(this) {
+			buf[in] = obj;
+			in = (in + 1) % size;
+		}
+
+		usedSlots.up();
 	}
 
-	public synchronized Object get() {
-		full.down();
-		Object obj = buf[out];
-		buf[out] = null;
-		out = (out + 1) % size;
-		empty.up();
+	public Object get() {
+		usedSlots.down();
+		
+		Object obj;
+		synchronized(this) {
+			obj = buf[out];
+			buf[out] = null;
+			out = (out + 1) % size;
+		}
+
+		emptySlots.up();
 		return (obj);
 	}
 
